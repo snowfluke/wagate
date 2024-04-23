a69ccb386f0141cfce2f247cc0256d8d87555e88
unknown <fazrinursyam@gmail.com>
2024-04-23 18:12:48 +0700

    debug

--- utils/cron.util.ts
+++ utils/cron.util.ts
@@ -1,10 +1,14 @@
 // bikin file cron.util.ts di folder utils
+import dotenv from "dotenv";
 import { CronJob } from "cron";
 import https from "https";
+dotenv.config();
 
 const fetch = require("node-fetch");
 const agent = new https.Agent({ keepAlive: false });
-const ENDPOINT =  process.env.ENDPOINT || "";
+const ENDPOINT = process.env.ENDPOINT || "";
+
+console.log("CRON ENDPOINT: ", ENDPOINT)
 
 const cron5min = new CronJob("*/5 * * * *", async () => {
   await fetch(ENDPOINT, {
